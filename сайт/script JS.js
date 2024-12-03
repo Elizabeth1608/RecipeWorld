a14b68364442d2915a2e98bebc19d47788957cd2
after <titova7655@gmail.com>
2024-12-04 00:00:40 +0700

    feature v2.0

---
+++ сайт/script JS.js
@@ -0,0 +1,32 @@
+document.getElementById('myButton1').addEventListener('click', function() {
+    // Переход на новую страницу
+    window.location.href = 'test 2.html';
+    
+    // Создание и добавление текста на новую страницу
+    localStorage.setItem('message', 'Чашушули..................');
+});
+
+document.getElementById('myButton2').addEventListener('click', function() {
+    // Переход на новую страницу
+    window.location.href = 'test 2.html';
+    
+    // Создание и добавление текста на новую страницу
+    localStorage.setItem('message', 'Пхали..................');
+});
+
+document.getElementById('myButton3').addEventListener('click', function() {
+    // Переход на новую страницу
+    window.location.href = 'test 2.html';
+    
+    // Создание и добавление текста на новую страницу
+    localStorage.setItem('message', 'Гозинаки................');
+});
+
+document.getElementById('myButton4').addEventListener('click', function() {
+    // Переход на новую страницу
+    window.location.href = 'test 2.html';
+    
+    // Создание и добавление текста на новую страницу
+    localStorage.setItem('message', 'Хачапури..................');
+});
+

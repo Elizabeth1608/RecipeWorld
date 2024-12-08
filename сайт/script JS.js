2cecacfbc7f63f0faf763ede6f8aba9ffd1aced3
Lisa <lisatishko@gmail.com>
2024-12-08 21:08:24 +0700

    feature: new recipe pages for japan

---
+++ сайт/script JS.js
@@ -0,0 +1,24 @@
+document.getElementById('myButton1').addEventListener('click', function() {
+    // Изменение текста на странице
+    document.getElementById('recipe-title').textContent = 'Чашушули';
+    document.getElementById('recipe-description').textContent = 'Ингредиенты: мясо, лук, помидоры, специи. Приготовление: Обжарьте мясо с луком, добавьте помидоры и специи. Тушите до готовности.';
+});
+
+document.getElementById('myButton2').addEventListener('click', function() {
+    // Изменение текста на странице
+    document.getElementById('recipe-title').textContent = 'Пхали';
+    document.getElementById('recipe-description').textContent = 'Ингредиенты: свекла, грецкие орехи, специи. Приготовление: Измельчите свеклу, смешайте с грецкими орехами и специями. Подавайте холодным.';
+});
+
+document.getElementById('myButton3').addEventListener('click', function() {
+    // Изменение текста на странице
+    document.getElementById('recipe-title').textContent = 'Гозинаки';
+    document.getElementById('recipe-description').textContent = 'Ингредиенты: грецкие орехи, мед, сахар. Приготовление: Обжарьте орехи и смешайте с медом и сахаром. Разлейте по формочкам и дайте застыть.';
+});
+
+document.getElementById('myButton4').addEventListener('click', function() {
+    // Изменение текста на странице
+    document.getElementById('recipe-title').textContent = 'Хачапури';
+    document.getElementById('recipe-description').textContent = 'Ингредиенты: тесто, сыр, яйца. Приготовление: Сформируйте тесто, добавьте начинку и запеките в духовке.';
+});
+
